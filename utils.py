619813823cbb442b35b17e0cdca5e78725e70203
pc2005 <philchen2005@gmail.com>
2020-06-08 21:23:09 -0400

    Added comment to util function.

--- utils.py
+++ utils.py
@@ -2,11 +2,19 @@ import os
 import zipfile
 import tempfile
 
-
 def get_gzipped_model_size(file):
+  """ gzip and get model size
+
+  Args:
+      file (str): File URL
+
+  Returns:
+      size (int): Size of gzipped file
+  """
   # Returns size of gzipped model, in bytes.
   _, zipped_file = tempfile.mkstemp('.zip')
   with zipfile.ZipFile(zipped_file, 'w', compression=zipfile.ZIP_DEFLATED) as f:
     f.write(file)
 
   return os.path.getsize(zipped_file)
+
